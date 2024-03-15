37e7ee1cbdee59922b2eca7ec85a28014314cd4c
Ashish <am7011414408@gmail.com>
2024-03-15 22:37:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+
+  return (
+    <>
+     <AddTodo/>
+     <Todos/>
+    </>
+  )
+}
+
+export default App
---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,23 @@
+import { createSlice,nanoid } from "@reduxjs/toolkit";
+
+const initialState={
+    todos:[{id:1,text:"hello by mishra"}]
+}
+export const todoSlice=createSlice({
+    name:"todo",
+    initialState,
+    reducers:{
+        addTodo:(state,action)=>{
+            const todo={
+                id:nanoid(),
+                text:action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo:(state,action)=>{
+            state.todos=state.todos.filter((todo)=>todo.id!==action.payload)
+        }
+    }
+})
+export const {addTodo,removeTodo}=todoSlice.actions
+export default todoSlice.reducer
